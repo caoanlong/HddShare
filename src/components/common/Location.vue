9cddb89f2169709220d3e75d8fdc56359a940826
coolia <gj@vfu56.com>
2017-11-28 12:00:45 +0800

    cc

--- src/components/common/Location.vue
+++ src/components/common/Location.vue
@@ -1,5 +1,6 @@
 <template>
 	<div v-if="showMap" class="mapWapper">
+		<div class="backBtn"><i></i></div>
 		<baidu-map class="map"
 			:center="position"
 			:zoom="22">
@@ -12,6 +13,28 @@
 			<!-- <bm-geolocation anchor="BMAP_ANCHOR_BOTTOM_RIGHT" :showAddressBar="true" :autoLocation="true" :locationIcon="{url: locationIcon, size: {width: 30, height: 52}, opts:{imageSize:{width: 30, height: 52}}}"></bm-geolocation> -->
 			<bm-navigation anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-navigation>
 		</baidu-map>
+		<transition name="slideUp">
+			<div class="LocationDetail" v-show="true">
+				<div class="baseInfo" to="">
+					<div class="ls">
+						<img width="70" src='../../assets/img/defaultImg.svg' alt=""/>
+						<p class="text-center status"><span class="status1"><i></i>运输中</span></p>
+						<!-- <p class="text-center status"><span class="status2"><i></i>空车</span></p> -->
+		        	</div>
+		        	<div class="text">
+						<p class="line bold">
+							<span class="from">江西吉安</span>
+							<span class="arrow"></span>
+							<span>深圳福田，深圳坪山</span>
+						</p>
+			           	<p class="truckInfo">张师傅/鲁A·25437</p>
+			            <p class="truckType">12.8米/仓栅式货车/不可装车</p>
+			            <p class="Location">4天前 深圳17m</p>
+		            </div>
+				</div>
+			    <a class=" icon-phone" href="'tel:'1000"></a>
+			</div>
+		</transition>
 	</div>
 </template>
 <script>
@@ -93,6 +116,22 @@
 	}
 </script>
 <style lang="stylus" scoped>
+	.backBtn
+		width 30px
+		height 30px
+		padding 7px
+		border-radius 4px
+		background rgba(0,0,0,.5)
+		position absolute
+		top 10px
+		left 10px
+		z-index 100
+		i
+			width 16px
+			height 16px
+			background url('../../assets/img/backBtn-icon.svg') no-repeat
+			display block
+			background-size cover
 	.map
 		position fixed
 		top 0
@@ -107,4 +146,95 @@
 			img
 				width 100%
 				height 100%
+	.LocationDetail
+		position fixed
+		bottom 0
+		z-index 100
+		width 100%
+		background #fff
+		padding 10px 50px 10px 90px
+		min-height 110px
+		.ls
+			position absolute
+			left 10px
+			top 10px
+			width 70px
+			img
+				border-radius 4px
+			.status
+				font-size 12px
+				text-align center
+				display block
+				height 20px
+				span
+					color #fff
+					height 20px
+					padding 2px 10px 2px 2px
+					border-radius 8px
+					i
+						height 16px
+						width 16px
+						display inline-block
+						vertical-align top
+						border-radius 8px
+						margin 1px 5px 0 0
+				.status1
+					background #6cc
+					i
+						background url('../../assets/img/status_icon1.svg') #fff no-repeat center
+						background-size 80%
+				.status2
+					background #f80
+					i
+						background url('../../assets/img/status_icon2.svg') #fff no-repeat center
+						background-size 80%
+		&.slideUp-enter-active
+			 transition all 3s ease
+			 height 50px
+			 overflow hidden
+		&.slideUp-leave-active
+			 transition all 3s ease
+			 height 0px
+			 overflow hidden
+		&.slideUp-enter
+		&.slideUp-leave
+			 height 0
+			 opacity 0
+		.icon-phone
+			right 15px
+			width 30px
+			height 30px
+			background-image url('../../assets/img/ic_call_phone_image.png')
+			background-size cover
+			display block
+			position absolute
+			top 50%
+			margin-top -15px
+		.text
+			p
+				color #999
+				margin-top 3px
+				font-size 14px
+			.line
+				font-size 16px
+				color #666
+				font-weight bold
+				ext-overflow ellipsis
+				white-space nowrap
+				overflow hidden
+				height 25px
+				margin-top 0
+				span
+					display inline-block
+					vertical-align top
+					height 16px
+				.arrow
+					background url('../../assets/img/area_arrow.png') no-repeat center
+					background-size contain
+					width 20px
+					vertical-align middle
+			.truckInfo
+				color #666
+			.truckType
+				color #6cc
 </style>
--- src/components/common/TruckIcon.vue
+++ src/components/common/TruckIcon.vue
@@ -45,14 +45,24 @@
         width 46px
         height 46px
         border-radius 23px
+        // background url('../../assets/img/location.svg') no-repeat
         background #fff
-        box-shadow 0 0 5px rgba(0,0,0,.5)
+        box-shadow 0 0 5px 0 rgba(0,0,0,.5)
         color #ddd
         text-align center
         padding 3px
         position absolute
+        z-index 10
+        &.selected
+            border 1px solid #f60
+            padding 2px
+            box-shadow 0 0 2px #f60
+            z-index 100
         img
             width 40px
             height 40px
-            border-radius 20px
+            border-radius 18px
+            position relative
+            z-index 20
+            
 </style>
