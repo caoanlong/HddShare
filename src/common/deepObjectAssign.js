ce2ba7da9556f7323a85762a982724358965b284
Caoanlong <caoanlong@gmail.com>
2018-01-04 10:05:12 +0800

    gg

--- src/common/deepObjectAssign.js
+++ src/common/deepObjectAssign.js
@@ -1,13 +1,13 @@
 import objectAssign from 'object-assign'
 
 const deepObjectAssign = function ($vm, options) {
-	const default = {}
+	const defaultPro = {}
 	for (let i in $vm.options.props) {
 		if (i != 'value') {
-			default[i] = $vm.options.props[i].default
+			defaultPro[i] = $vm.options.props[i].default
 		}
 	}
-	const _option = objectAssign({}, default, options)
+	const _option = objectAssign({}, defaultPro, options)
 	for (let i in _option) {
 		$vm[i] = _option[i]
 	}
