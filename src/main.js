85adf736c4b797aeab5d72b8fc37270d695edc6c
Caoanlong <caoanlong@gmail.com>
2018-06-27 18:03:20 +0800

    gg

--- src/main.js
+++ src/main.js
@@ -12,31 +12,7 @@ Vue.use(vueResource)
 Vue.use(_global)
 Vue.use(msg)
 
-// Vue.http.interceptors.push((request, next) => {
-    //请求发送前的处理逻辑
-    // request.headers['JSESSIONID'] = '216d2847-b77e-4436-b7dd-6f79d837816c'
-    // request.headers.set('HDD-Client-Tag', '216d2847-b77e-4436-b7dd-6f79d837816c')
-   //  request.headers.set('HDD_API_CURRENT_USER', '')
-   //  const authorization = localStorage.getItem('authorization');
-   //  if (authorization) {
-   //    	request.headers.set('Authorization', authorization);
-   //  }
-  	// next();
-// });
-
 Vue.config.productionTip = false
-
-// Vue.http.interceptors.push((request, next) => {
-//     let token = localStorage.getItem('token')
-//     request.headers.set('HDD_API_CURRENT_USER', 'aaaa')
-//     next((resbonse) => {
-//         if (resbonse.body.code == 1001 || resbonse.body.code == 1002 || resbonse.body.code == 1003) {
-//             localStorage.removeItem('token')
-//             window.location.href = '/FKCFront/#/login'
-//             // window.location.href = '/#/login'
-//         }
-//     })
-// })
 
 Vue.use(BaiduMap, {
   // ak 是在百度地图开发者平台申请的密钥 详见 http://lbsyun.baidu.com/apiconsole/key */
--- src/router/index.js
+++ src/router/index.js
@@ -3,118 +3,145 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
 	// mode: 'history',
 	routes: [
 	    {
 	    	path: '/',
-	    	name: 'Home',
+			name: 'Home',
 	    	component: resolve => require(['@/components/Home'], resolve)
 	    },
 	    {
 	      	path: '/AppDownload',
-	      	name: 'AppDownload',
+			name: 'AppDownload',
+			meta: { title: 'App下载' },
 	      	component: resolve => require(['@/components/AppDownload'], resolve)
 	    },
 	    {
 	      	path: '/ConsignorDownload',
-	      	name: 'ConsignorDownload',
+			name: 'ConsignorDownload',
+			meta: { title: '货主下载' },
 	      	component: resolve => require(['@/components/ConsignorDownload'], resolve)
 	    },
 	    {
 	      	path: '/DriverDownload',
-	      	name: 'DriverDownload',
+			name: 'DriverDownload',
+			meta: { title: '司机下载' },
 	      	component: resolve => require(['@/components/DriverDownload'], resolve)
 	    },
 	    {
 	      	path: '/goodsDetail',
-	      	name: 'goodsDetail',
+			name: 'goodsDetail',
+			meta: { title: '货源详情' },
 	      	component: resolve => require(['@/components/goodsDetail'], resolve)
 	    },
 	    {
 	      	path: '/truckDetail',
-	      	name: 'truckDetail',
+			name: 'truckDetail',
+			meta: { title: '车辆详情' },
 	      	component: resolve => require(['@/components/truckDetail'], resolve)
 	    },
 	    {
 	      	path: '/agreement',
-	      	name: 'agreement',
+			name: 'agreement',
+			meta: { title: '协议' },
 	      	component: resolve => require(['@/components/agreement'], resolve)
 	    },
 	    {
           	path: '/infoFee',
-          	name: 'infoFee',
+			name: 'infoFee',
+			meta: { title: '信息费协议' },
           	component: resolve => require(['@/components/infoFee'], resolve)
         },
         {
           	path: '/transFee',
-          	name: 'transFee',
+			name: 'transFee',
+			meta: { title: '运输费协议' },
           	component: resolve => require(['@/components/transFee'], resolve)
         },
         {
           	path: '/statement',
-          	name: 'statement',
+			name: 'statement',
+			meta: { title: '信息费交易声明' },
           	component: resolve => require(['@/components/statement'], resolve)
         },
 	    {
 	      	path: '/error',
-	      	name: 'error',
+			name: 'error',
+			meta: { title: '错误' },
 	      	component: resolve => require(['@/components/error'], resolve)
 	    },
 	    {
 	      	path: '/updata',
-	      	name: 'updata',
+			name: 'updata',
+			meta: { title: '版本更新' },
 	      	component: resolve => require(['@/components/updata'], resolve)
 	    },
 	    {
 	      	path: '/findGoods',
-	      	name: 'findGoods',
+			name: 'findGoods',
+			meta: { title: '寻找货源' },
 	      	component: resolve => require(['@/components/findGoods'], resolve)
 		},
 		{
 			path: '/goodsLine',
 			name: 'goodsLine',
+			meta: { title: '查看线路' },
 			component: resolve => require(['@/components/goodsLine'], resolve)
 	  	},
 	    {
 	      	path: '/findTruck',
-	      	name: 'findTruck',
+			name: 'findTruck',
+			meta: { title: '寻找车源' },
 	      	component: resolve => require(['@/components/findTruck'], resolve)
 		},
 		{
 			path: '/findTruckByMap',
 			name: 'findTruckByMap',
+			meta: { title: '地图找车' },
 			component: resolve => require(['@/components/findTruckByMap'], resolve)
 	  	},
 		{
 			path: '/truckLocation',
 			name: 'truckLocation',
+			meta: { title: '查看位置' },
 			component: resolve => require(['@/components/truckLocation'], resolve)
 	  	},
 	    {
 	      	path: '/aboutHdd',
-	      	name: 'aboutHdd',
+			name: 'aboutHdd',
+			meta: { title: '关于货多多' },
 	      	component: resolve => require(['@/components/aboutHdd'], resolve)
 	    },
 	    {
 	      	path: '/bankList',
-	      	name: 'bankList',
+			name: 'bankList',
+			meta: { title: '支持银行列表' },
 	      	component: resolve => require(['@/components/bankList'], resolve)
 	    },
 	    {
 	      	path: '/FaqList',
-	      	name: 'FaqList',
+			name: 'FaqList',
+			meta: { title: '常见问题' },
 	      	component: resolve => require(['@/components/FaqList'], resolve)
 	    },
 	    {
 	      	path: '/FaqDetail',
-	      	name: 'FaqDetail',
+			name: 'FaqDetail',
+			meta: { title: '常见问题详情' },
 	      	component: resolve => require(['@/components/FaqDetail'], resolve)
 	    },
 	    {
 	      	path: '/PaymentAgreement',
-	      	name: 'PaymentAgreement',
+			name: 'PaymentAgreement',
+			meta: { title: '支付协议' },
 	      	component: resolve => require(['@/components/PaymentAgreement'], resolve)
 	    }
   	]
 })
+
+router.beforeEach((to, from, next) => {
+	document.title = to.meta.title
+})
+
+export default router
