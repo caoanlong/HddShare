408e31866936c2af8cdc7b5f5dbf491ed42c8ad6
Caoanlong <caoanlong@gmail.com>
2018-09-18 13:57:44 +0800

    添加request

---
+++ src/pages/Home/linkList.js
@@ -0,0 +1,104 @@
+export default [
+    {
+        title: 'App下载',
+        pathname: '/AppDownload',
+        icon: require('../../assets/img/download.svg')
+    },
+    {
+        title: '货主下载',
+        pathname: '/ConsignorDownload',
+        icon: require('../../assets/img/download.svg')
+    },
+    {
+        title: '司机下载',
+        pathname: '/DriverDownload',
+        icon: require('../../assets/img/download.svg')
+    },
+    {
+        title: '关于货多多',
+        pathname: '/aboutHdd',
+        icon: require('../../assets/img/about.svg')
+    },
+    {
+        title: '注册协议',
+        pathname: '/agreement',
+        icon: require('../../assets/img/agreement.svg')
+    },
+    {
+        title: '信息费协议',
+        pathname: '/infoFee',
+        icon: require('../../assets/img/agreement.svg')
+    },
+    {
+        title: '运输费协议',
+        pathname: '/transFee',
+        icon: require('../../assets/img/download.svg')
+    },
+    {
+        title: '支付协议',
+        pathname: '/PaymentAgreement',
+        icon: require('../../assets/img/PaymentAgreement.svg')
+    },
+    {
+        title: '货源详情',
+        pathname: '/goodsDetail',
+        icon: require('../../assets/img/detail.svg')
+    },
+    {
+        title: '车辆详情',
+        pathname: '/truckDetail',
+        icon: require('../../assets/img/detail.svg')
+    },
+    {
+        title: '寻找货源',
+        pathname: '/findGoods',
+        icon: require('../../assets/img/goods_icon.svg')
+    },
+    {
+        title: '寻找车辆',
+        pathname: '/findTruck',
+        icon: require('../../assets/img/truck_icon.svg')
+    },
+    {
+        title: '银行列表',
+        pathname: '/bankList',
+        icon: require('../../assets/img/bank.svg')
+    },
+    {
+        title: '提现手续费',
+        pathname: '/FaqDetail',
+        search: '?code=ServiceCharge',
+        icon: require('../../assets/img/charge.svg')
+    },
+    {
+        title: '提现规则',
+        pathname: '/FaqDetail',
+        search: '?code=Withdrawals',
+        icon: require('../../assets/img/rules.svg')
+    },
+    {
+        title: '常见问题',
+        pathname: '/FaqList',
+        icon: require('../../assets/img/help.svg')
+    },
+    {
+        title: '交易申明',
+        pathname: '/statement',
+        icon: require('../../assets/img/statement.svg')
+    },
+    {
+        title: '版本更新',
+        pathname: '/updata',
+        icon: require('../../assets/img/updata_icon.svg')
+    },
+    {
+        title: '404页面',
+        pathname: '/error',
+        icon: require('../../assets/img/404.svg')
+    },
+    {
+        title: '敬请期待',
+        pathname: '',
+        icon: require('../../assets/img/more.svg')
+    },
+]
