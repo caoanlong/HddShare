f9770b2ddc71a26082f1bfd3bc636ce36525ea7f
Caoanlong <caoanlong@gmail.com>
2018-09-18 09:52:06 +0800

    创建react版本

--- config/prod.env.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	NODE_ENV: '"production"',
-	ENV_CONFIG: '"prod"',
-	BASE_API: '"//api.hdd56.com/hdd/"',  // 生产
-	IMG_API: '"//image.hdd56.com/"'
-}
--- config/test.env.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	NODE_ENV: '"production"',
-	ENV_CONFIG: '"test"',
-	BASE_API: '"//develop.we-service.cn/test-hdd/v2/"',  // 测试
-	// BASE_API: '"//develop.we-service.cn/yj/api/"',  // 测试
-	IMG_API: '"//develop.we-service.cn/hdd/image/"'
-}
