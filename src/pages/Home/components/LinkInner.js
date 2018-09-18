889e15fbc7465840e60807c2a23ad33f9dd9f8ca
Caoanlong <caoanlong@gmail.com>
2018-09-18 14:11:43 +0800

    gg

---
+++ src/pages/Home/components/LinkInner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import style from './LinkInner.scss'
+
+export default props => (
+    <Link className={style.grid} to={{pathname: props.pathname, search: props.search}}>
+        <div className={style.grid_icon}>
+            <img alt={props.title} src={props.icon} />
+        </div>
+        <div className={style.grid_label}>{props.title}</div>
+    </Link>
+)
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,7 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 import style from './style.scss'
 import linkList from './linkList'
-
-const LinkInner = props => (
-    <Link className={style.grid} to={{pathname: props.pathname, search: props.search}}>
-        <div className={style.grid_icon}>
-            <img alt={props.title} src={props.icon} />
-        </div>
-        <div className={style.grid_label}>{props.title}</div>
-    </Link>
-)
+import LinkInner from './components/LinkInner'
 
 class Home extends Component {
     render() {
