6b8186f64e2f296dcf053892c8cad17c2135770d
coolia <gj@vfu56.com>
2018-10-19 17:49:09 +0800

    ccccc

--- src/pages/Home/components/LinkInner/index.jsx
+++ src/pages/Home/components/LinkInner/index.jsx
@@ -4,8 +4,8 @@ import style from './style.scss'
 
 export default props => (
     <Link className={style.linkGrid} to={{pathname: props.pathname, search: props.search}}>
-        <div className={style.icon}>
-            <img alt={props.title} src={props.icon} />
+        <div className={style.iconBox}>
+            <img alt={props.title} src={props.icon} className={style.icon}/>
         </div>
         <div className={style.label}>{props.title}</div>
     </Link>
--- src/pages/InfoFee/index.jsx
+++ src/pages/InfoFee/index.jsx
@@ -1,53 +1,101 @@
-
 import React, { Component } from "react";
 import qs from "qs";
-import style from './style.scss'
+import style from "./style.scss";
 import Content from "../../api/Content";
+import AgentOrder from "../../api/AgentOrder";
 
 class InfoFee extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      content: ''
-    };
-    this.query = qs.parse(this.props.location.search, {
-      ignoreQueryPrefix: true
-    });
-  }
-  componentWillMount() {
-    this.getContent();
-  }
-  getContent() {
-    Content.getContent({
-      code: this.query.code || "InformationFees",
-      AppId: this.query.AppId || "",
-      //   AppId: this.query.AppId || "10000003",
-      Authorization: this.query.Authorization
-    }).then(res => {
-      this.setState({ content: res[0].content });
-    });
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			content: "",
+			infoData: {}
+		};
+		this.query = qs.parse(this.props.location.search, {
+			ignoreQueryPrefix: true
+		});
+	}
+	componentWillMount() {
+		this.getDetail();
+		this.getContent();
+	}
+	getDetail() {
+		AgentOrder.findById({
+			agentAgreementID: this.query.agentAgreementID,
+			AppId: this.query.AppId || "",
+			Authorization: this.query.Authorization
+		}).then(res => {
+			this.setState({ infoData: res[0].content });
+		});
+	}
+	getContent() {
+		Content.getContent({
+			code: this.query.code || "InformationFees",
+			AppId: this.query.AppId || "",
+			//   AppId: this.query.AppId || "10000003",
+			Authorization: this.query.Authorization
+		}).then(res => {
+			this.setState({ content: res[0].content });
+		});
+	}
 
 	render() {
 		return (
 			<div className={style.container}>
-        		<div className="padd">
-          			<div className="item">
-						<img className="icon" src="../assets/img/position_icon.svg">
-						<div className="lineInfo">
-						<span className="start">{{ infoData.areaFromName }}</span>
-						<span className="arrow"></span>
-						<span className="end">{{ infoData.areaToName }}</span>
+				<div className={style.padd}>
+					<div className={style.item}>
+						<img
+							className={style.icon}
+							src={require("../../assets/img/position_icon.svg")}
+							alt=""
+						/>
+						<div className={style.lineInfo}>
+							<span className={style.start}>
+								{this.state.infoData.areaFromName}{" "}
+							</span>
+							<span className={style.arrow} />
+							<span className={style.end}>
+								{this.state.infoData.areaToName}{" "}
+							</span>
+						</div>
+					</div>
+					<div className={style.item}>
+						<img className={style.icon} src={require("../../assets/img/detail_icon2.svg")} alt=""/>
+						<div className={style.goodInfo}>
+							{this.state.infoData.cargoName
+								? this.state.infoData.cargoName + "/"
+								: ""}
+							{this.state.infoData.cargoWeight
+								? this.state.infoData.cargoWeight + "吨"
+								: ""}
+							{this.state.infoData.cargoVolume
+								? "/" + this.state.infoData.cargoVolume + "方"
+								: ""}
+							{this.state.infoData.cargoNum
+								? "/" + this.state.infoData.cargoNum + "件"
+								: ""}
+						</div>
+					</div>
+					<img className={style.success_icon} src={require("../../assets/img/success_icon.png")} alt=""/>
+				</div>
+				<div className={style.pannel}>
+					<div className={style.pannel_item}>
+						<label>流水单号</label>{this.state.infoData.agreementCode}
+					</div>
+					<div className={style.pannel_item}>
+						<label>付款时间</label>
+						{this.state.infoData.orderTime}
+					</div>
+					<div className={style.pannel_item}>
+						<label>协商信息费</label>
+						<b className={style.c2}>
+							{this.state.infoData.infoFee ? this.state.infoData.infoFee + "元" : ""}
+						</b>
 					</div>
 				</div>
-            	<div className="item">
-              		<img className="icon" src="../assets/img/detail_icon2.svg">
-                		<div className="goodInfo">{{ infoData.cargoName ? infoData.cargoName + '/' : '' }}{{ infoData.cargoWeight ? infoData.cargoWeight + '吨' : '' }}{{ infoData.cargoVolume ? '/' + infoData.cargoVolume + '方' : '' }}{{ infoData.cargoNum ? '/' + infoData.cargoNum + '件' : '' }}</div>
-				</div>
-				<img className="success_icon" src="../assets/img/success_icon.png" />
 			</div>
-    );
-  }
+		);
+	}
 }
 
-export default InfoFee
+export default InfoFee;
